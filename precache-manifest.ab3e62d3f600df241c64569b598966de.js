f25951b69d9ea8504bfc679242710792923d80c5
Esther1990 <esther.fiddler.1990@gmail.com>
2019-03-06 04:16:42 +0200

    Updates

---
+++ precache-manifest.ab3e62d3f600df241c64569b598966de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c334a69b738a3125551",
+    "url": "/orbit-game-app/static/js/runtime~main.6c334a69.js"
+  },
+  {
+    "revision": "d3887716e3c2b7321e82",
+    "url": "/orbit-game-app/static/js/main.d3887716.chunk.js"
+  },
+  {
+    "revision": "c51d20d6ac4776f012ae",
+    "url": "/orbit-game-app/static/js/2.c51d20d6.chunk.js"
+  },
+  {
+    "revision": "d3887716e3c2b7321e82",
+    "url": "/orbit-game-app/static/css/main.dd87863d.chunk.css"
+  },
+  {
+    "revision": "cef9c69ca57da3111a2e281fb19833f1",
+    "url": "/orbit-game-app/index.html"
+  }
+];
